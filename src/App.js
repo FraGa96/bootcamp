4aa52ed17fb079a6703c9dd1073a54080500d2c8
FraGa96 <33188801+FraGa96@users.noreply.github.com>
2021-05-06 12:08:15 -0500

    Semana 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import Counter from './components/counter/Counter';
+import Counter from './components/counter/CounterWithHooks';
 import './App.css';
 
 function App() {
+  const sayHello = () => {
+    console.log("HELLOO!");
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Counter maxValue={15}/>
+        <Counter maxValue={15} sayHi={sayHello}>
+          <span>Hey you!</span>
+        </Counter>
       </header>
     </div>
   );
